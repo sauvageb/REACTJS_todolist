89d690c0bcaccf2377b7d8f6383eb905e431415d
sauvageb <sauvageboris.pro@gmail.com>
2023-09-14 09:53:14 +0200

    init project

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+export default function App() {
+
   return (
     <div className="App">
       <header className="App-header">
@@ -20,6 +21,6 @@ function App() {
       </header>
     </div>
   );
+
 }
 
-export default App;
